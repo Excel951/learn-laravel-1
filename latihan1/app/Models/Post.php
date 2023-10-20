14212f739e7ddfc2e051941876da3926df3d2a8e
Excel951 <abigailexcelsisdeo@gmail.com>
2023-10-20 13:55:02 +0700

    add eloquent relationships

--- latihan1/app/Models/Post.php
+++ latihan1/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -17,6 +18,10 @@ class Post extends Model
         'excerpt',
         'body',
     ];
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
 }
 
 // [
--- latihan1/resources/views/post.blade.php
+++ latihan1/resources/views/post.blade.php
@@ -4,6 +4,8 @@
 <article>
     <h2>{{ $article->title }}</h2>
     
+    <h5>By Deo in <a href="/categories/{{$article->category->slug}}">{{ $article->category->name }}</a></h5>
+
     {{-- Menjalankan php echo tanpa escape character (tag html dijalankan)--}}
     {!! $article->body !!}
 </article>
